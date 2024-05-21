66265b5786148cc5f18861045c94e1bc34e55cc4
MagnusMeyer41 <magnus.meyer@bredvid.no>
2024-05-21 10:50:52 +0200

    sat up logging to test and see getarounds requests

--- HttpTriggerGetAroundTest.cs
+++ HttpTriggerGetAroundTest.cs
@@ -1,12 +1,18 @@
-using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Functions.Worker;
 
 namespace GetAroundBredvid.Function
 {
     public class HttpTriggerGetAroundTest
     {
+
+        private static readonly string SecretToken = Environment.GetEnvironmentVariable("WEBHOOK_SECRET_TOKEN");
         private readonly ILogger<HttpTriggerGetAroundTest> _logger;
 
         public HttpTriggerGetAroundTest(ILogger<HttpTriggerGetAroundTest> logger)
@@ -15,10 +21,55 @@ namespace GetAroundBredvid.Function
         }
 
         [Function("HttpTriggerGetAroundTest")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post",  Route = null)] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("Function initialized.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            string signature = req.Headers["X-Drivy-Signature"];
+
+            foreach (var header in req.Headers)
+            {
+                _logger.LogInformation($"Header: {header.Key} = {string.Join(",", header.Value)}");
+            }
+
+            _logger.LogInformation("x-Drivy-Signature: " + signature);
+            _logger.LogInformation("requestBody: " + requestBody);
+
+            // if (!VerifySignature(requestBody, signature))
+            // {
+            //     _logger.LogWarning("Signature mismatch. Possible tampering detected.");
+            //     return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            // }
+
+            // var payload = JObject.Parse(requestBody);
+
+            // _logger.LogInformation($"Payload received: {payload.ToString()}");
+
+            // return new OkObjectResult("Sent in signature is: " + signature);
+            return new OkResult();
         }
+
+        private static bool VerifySignature(string payloadBody, string signature)
+        {
+            using (var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(SecretToken)))
+            {
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(signature));
+                var hashString = "sha1=" + BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+                return SlowEquals(hashString, signature);
+            }
+        }
+
+        private static bool SlowEquals(string a, string b)
+        {
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+
     }
 }
